6a203a72be12a010ec0255aa73dc0a38a46dae07
ytfh423-lab <ytfh423-lab@users.noreply.github.com>
2026-04-16 17:45:53 +0800

    feat: publish article team无资格强开

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2026/04/01/hello-world/"];function toRandomPost(){
+var posts=["2026/04/01/hello-world/","2026/04/16/team无资格强开/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
